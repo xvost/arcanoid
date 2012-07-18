e3ff5cca34231f1cb5bc44cf1a618440f47670eb
Andre <xvostt@gmail.com>
2012-07-18 20:41:51 +0300

    переход на ООП

--- arcV2.py
+++ arcV2.py
@@ -1,15 +1,36 @@
 __author__ = 'Xvost'
-import   pygame, color
+import   pygame
 
 class BALL:
-    self.x = 0
-    self.y = 0
-    self.X_vect = 0
-    self.Y_vect = 0
-    self.radius = 5
-    self.color = color.black
+    x = 0
+    y = 0
+    X_vect = 0
+    Y_vect = 0
+    radius = 5
+    color = color.black
 
     def __init__(self, Surface, color, pos, radius):
         self.x = pos[0]
         self.y = pos[1]
         self.rect = pygame.draw.circle(Surface, color, pos, radius)
+
+
+class window:
+    def __init__(self, size, background):
+        self.size = size
+        self.background = background
+        self.display = pygame.display.set_mode(size)
+        self.background = self.background.convert()
+        self.back_rect = self.background.get_rect(topleft=(0, 0))
+
+    def get_size(self):
+        return self.size
+
+    def __call__(self, *args, **kwargs):
+        self.display.blit(self.back_rect, self.display)
+        self.display.flip()
+
+background = pygame.image.load('disp.jpg')
+Main = window((300, 500), background)
+while 1:
+    Main()
